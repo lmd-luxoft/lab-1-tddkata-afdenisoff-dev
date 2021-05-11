5d53c94a15c222836e536492c9aa2ca189603895
Денисов Александр <afdenisoff@gmail.com>
2021-05-11 20:31:49 +0600

    Test push

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -1,14 +1,70 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TDDKata
 {
     internal class StringCalc
     {
+        private const char separator = ',';
+
         internal int Sum(string v)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(v))
+            {
+                return 0;
+            }
+
+            var intArray = GetIntArray(v);
+
+            if (!Validate(intArray))
+            {
+                return -1;
+            }
+
+            var result = 0;
+
+            foreach (var item in intArray)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+
+
+        private int[] GetIntArray(string source)
+        {
+            var result = new List<int>();
+
+            foreach (var arg in source.Split(separator))
+            {
+                if (!int.TryParse(arg, out int intValue))
+                {
+                    return Array.Empty<int>();
+                }
+
+                result.Add(intValue);
+            }
+
+            return result.ToArray();
+        }
+
+        private bool Validate(IEnumerable<int> source)
+        {
+            if (source.Count() == 0 || source.Count() > 2)
+            {
+                return false;
+            }
+
+            if (source.Any(value => value < 0))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -10,11 +10,107 @@ namespace TDDKata
     public class TestClass
     {
         [Test]
-        public void SimpleTest()
+        public void SumTwoPositiveDigitsShouldBeCorrectResult()
         {
+            //Arrange
             StringCalc calc = new StringCalc();
+            
+            //Act
             int value = calc.Sum("2,2");
+            
+            //Assert
             Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumOnePositiveDigitsShouldBeCorrectResult()
+        {
+            //Arrange
+            StringCalc calc = new StringCalc();
+
+            //Act
+            int value = calc.Sum("2");
+
+            //Assert
+            Assert.That(value, Is.EqualTo(2), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumEmptyShouldBeCorrectResult()
+        {
+            //Arrange
+            StringCalc calc = new StringCalc();
+
+            //Act
+            int value = calc.Sum("");
+
+            //Assert
+            Assert.That(value, Is.EqualTo(0), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumManyPositiveDigitsShouldBeErrorCode()
+        {
+            //Arrange
+            StringCalc calc = new StringCalc();
+
+            //Act
+            int value = calc.Sum("2,2,2");
+
+            //Assert
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumOneNegativeDigitsShouldBeErrorCode()
+        {
+            //Arrange
+            StringCalc calc = new StringCalc();
+
+            //Act
+            int value = calc.Sum("-2");
+
+            //Assert
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumTwoNegativeDigitsShouldBeErrorCode()
+        {
+            //Arrange
+            StringCalc calc = new StringCalc();
+
+            //Act
+            int value = calc.Sum("-2, -2");
+
+            //Assert
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumManyNegativeDigitsShouldBeErrorCode()
+        {
+            //Arrange
+            StringCalc calc = new StringCalc();
+
+            //Act
+            int value = calc.Sum("-2, -2, -3");
+
+            //Assert
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumTwoPositiveDigitsWithBadSeparatorShouldBeErrorCode()
+        {
+            //Arrange
+            StringCalc calc = new StringCalc();
+
+            //Act
+            int value = calc.Sum("2;2");
+
+            //Assert
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
         }
     }
 }
